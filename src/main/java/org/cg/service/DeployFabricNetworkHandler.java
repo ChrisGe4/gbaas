e204ff5a9d75ca4504586ec146fdd11d2e882502
chrisge <chris.ge@live.com>
2018-04-22 23:08:16 -0400

    rest v1 complete

--- src/main/java/org/cg/service/DeployFabricNetworkHandler.java
+++ src/main/java/org/cg/service/DeployFabricNetworkHandler.java
@@ -1,18 +1,18 @@
 package org.cg.service;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.cg.config.AppConfiguration;
 import org.cg.pojo.NetworkConfig;
+import org.cg.pojo.Property;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -23,31 +23,60 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/gbaas")
 public class DeployFabricNetworkHandler {
 
-  private static final Logger log = LoggerFactory.getLogger(DeployFabricNetworkHandler.class);
-  private final AppConfiguration properties;
-  private final DeploymentService service;
-  private final ObjectMapper objectMapper;
+    private static final Logger log = LoggerFactory.getLogger(DeployFabricNetworkHandler.class);
+    private final AppConfiguration properties;
+    private final DeploymentService service;
+    private final ObjectMapper objectMapper;
 
-  @Autowired
-  public DeployFabricNetworkHandler(AppConfiguration properties, DeploymentService service,
-      ObjectMapper objectMapper) {
-    this.properties = properties;
-    this.service = service;
-    this.objectMapper = objectMapper;
-  }
+    @Autowired
+    public DeployFabricNetworkHandler(AppConfiguration properties, DeploymentService service,
+        ObjectMapper objectMapper) {
+        this.properties = properties;
+        this.service = service;
+        this.objectMapper = objectMapper;
+    }
 
-  @PostMapping(value = "/fabric", headers = "Accept=application/json")
-  public String deployFabricNetwork(@RequestParam("install_everything") String ifInstall,
-      @RequestBody NetworkConfig config) {
+    @PostMapping(value = "/fabric", headers = "Accept=application/json")
+    public String deployFabricNetwork(@RequestParam("create_instance") Boolean createInstance,
+        @RequestParam("install_software") Boolean installSoftware,
+        @RequestParam("deploy_composer") Boolean deployComposer,
+        @RequestBody NetworkConfig config) {
 
-    return properties.DOMAIN;
+        Map<String, Map<String, String>> orgNameIpMap =
+            service.deployFabric(config, createInstance, installSoftware);
+        if (deployComposer) {
+            service.deployComposer(config, orgNameIpMap);
+        }
 
-  }
+        service.runScript();
+        return "succeed";
+    }
 
-  @GetMapping("/hello-world")
-  public String sayHello(
-  ) {
-    return properties.DOMAIN + " - " + properties.CA_PORT;
-  }
+
+    @PostMapping(value = "/composer", headers = "Accept=application/json")
+    public String deployComposer(@RequestBody NetworkConfig config) {
+        service.deployComposer(config, service.getInstanceNameIPMap(config));
+        service.runScript();
+        return "succeed";
+    }
+
+    @GetMapping("/hello-world")
+    public String sayHello() throws JsonProcessingException {
+
+        Property property1 = new Property();
+        property1.setOrg("google");
+        property1.setNumOfPeers(2);
+        Property property2 = new Property();
+        property2.setOrg("boa");
+        property2.setNumOfPeers(2);
+        NetworkConfig config = new NetworkConfig();
+        config.setGcpProjectName("hyperledger-poc");
+        config.setOrdererName("orderer-google-boa");
+        config.setGcpZoneName("us-east1-b");
+        config.setChannelName("common");
+        config.setProperties(List.of(property1, property2));
+
+        return objectMapper.writeValueAsString(config);
+    }
 
 }
